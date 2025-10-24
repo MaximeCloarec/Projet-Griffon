b6db575043ceadf6154f3e8045ed30341f11a175
MaximeCloarec <maxime.cloarec@yahoo.fr>
2025-10-24 16:37:09 +0200

    removed socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,8 @@
 //Variables environment
 require("dotenv").config({ path: __dirname + "/.env" });
 
-
-//Server creation
-const http = require("http");
-const app = require("./app"); // Import the Express app
-const { Server } = require("socket.io");
-
 const PORT = process.env.PORT || 3000;
-
-const server = http.createServer(app); // Create an HTTP server with the Express app
-const io = new Server(server, {
-    cors: {
-        origin: "*", // Allow all origins for CORS
-    },
-});
-
-
-//Socket IO setup
-io.on("connection", (socket) => {
-    //Event for connection
-    console.log("A user connected:", socket.id);
-
-    socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
-    });
-});
+const server = require("http").createServer(require("./app"));
 
 //Server listening
 server
